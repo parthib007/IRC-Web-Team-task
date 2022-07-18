834c7d972654c38e9ef3fb6baa0e9eb4ab7205db
unknown <parthibdas007@gmail.com>
2022-07-18 20:04:27 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// const all_universities = document.getElementById('all_universities');
+// const northAmerica_universities = document.getElementById('northAmerica_universities');
+// northAmerica_universities.onclick = function(){
+//     if(northAmerica_universities.style.display !== 'none'){
+//         northAmerica_universities.style.display = 'none';
+//     }
+//     else{
+//         northAmerica_universities.style.display = 'block';
+//     }
+// };
